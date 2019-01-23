df5b79fa070c8a9fe17f71fde7fef2020b126789
Greg Morgan <gregm9999@gmail.com>
2019-01-23 03:03:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function showCommits(el) {
 }
 
 function renderCommits(data) {
-  data.map(result => renderCommit(result).join(""))
+  let result=data.map(commit => renderCommit(result).join(""))
   return `<ul>${result}</ul>`
 }
 
