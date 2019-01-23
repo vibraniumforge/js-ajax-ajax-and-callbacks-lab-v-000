a7eec5446946736456e5a82143cef7ac994ca0c6
Greg Morgan <gregm9999@gmail.com>
2019-01-23 02:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function renderSearchResult(result) {
   return `
     <div>
       <h3><a href="${result.html_url}">${result.name}</a></h3>
-      <p href="#" data-repository="${result.name}" data-owner="${result.owener.login}" onclick="showCommits(this)"
+      <p href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)"
     </div>
     <hr>
     `
